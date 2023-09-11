60d34e3a1923afbe9fb437db418973df5f301c45
u632763 <mdfuenmayorsoto@teco.com.ar>
2023-09-11 12:19:49 -0300

    fix types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as PixelsImage } from './PixelsImage';
 export type { PixelsImageProps } from "./types"
-export type { PixelsImageSource } from "node-pixels"
-export { getImageSource } from "node-pixels";
+export type { PixelsImageSource, EXPORT_OBJECT } from "node-pixels"
+export { getImageSource, getExportObject } from "node-pixels";
