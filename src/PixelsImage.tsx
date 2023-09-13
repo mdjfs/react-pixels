828237e6499a561e28946fe98c74a9d0a7831fb9
u632763 <mdfuenmayorsoto@teco.com.ar>
2023-09-13 09:42:39 -0300

    export object change

--- src/PixelsImage.tsx
+++ src/PixelsImage.tsx
@@ -1,53 +1,63 @@
-import React, { createRef, useEffect, useState } from 'react';
+import React, { createRef, useEffect, useState } from "react";
 import { FILTERS, PixelsImageData, PixelsImageSource } from "node-pixels";
-import { adjustColors,drawImageSource, getExportObject, getImageSource, loadFilter, setHorizontalFlip, setVerticalFlip, applyChanges } from 'node-pixels';
-import { EDIT_OBJECT, PixelsImageProps } from './types';
+import {
+  adjustColors,
+  drawImageSource,
+  getExportObject,
+  getImageSource,
+  loadFilter,
+  setHorizontalFlip,
+  setVerticalFlip,
+  applyChanges,
+} from "node-pixels";
+import { EDIT_OBJECT, PixelsImageProps } from "./types";
 
-const PixelsImage: React.FC<PixelsImageProps> = ({ 
-  onFilter, 
-  type, 
-  filter, 
-  brightness, 
-  saturation, 
-  contrast, 
-  verticalFlip, 
-  horizontalFlip, 
-  src, 
-  ...props }) => {
+const PixelsImage: React.FC<PixelsImageProps> = ({
+  onFilter,
+  type,
+  filter,
+  brightness,
+  saturation,
+  contrast,
+  verticalFlip,
+  horizontalFlip,
+  src,
+  ...props
+}) => {
   const ref = createRef<HTMLCanvasElement>();
-  const [editObject, setEditObject] = useState<EDIT_OBJECT>({})
+  const [editObject, setEditObject] = useState<EDIT_OBJECT>({});
   const [isVisible, setIsVisible] = useState(false);
   const [source, setSource] = useState<PixelsImageSource>();
   const [data, setData] = useState<PixelsImageData>();
   const [isFiltered, setIsFiltered] = useState(false);
 
   const load = async () => {
-    if(data && ref.current) {
+    if (data && ref.current) {
       const canvas = ref.current;
       let changed = false;
       let { context, imageData } = data;
       setIsFiltered(true);
-      if(editObject.filter) {
+      if (editObject.filter) {
         await loadFilter(imageData, editObject.filter as FILTERS);
         changed = true;
       }
       const { brightness, saturation, contrast } = editObject;
-      if(brightness || saturation || contrast) {
+      if (brightness || saturation || contrast) {
         await adjustColors(imageData, { brightness, saturation, contrast });
         changed = true;
       }
-      if(changed) applyChanges(imageData, context);
-      if(editObject.verticalFlip) {
+      if (changed) applyChanges(imageData, context);
+      if (editObject.verticalFlip) {
         setVerticalFlip(canvas, context);
         changed = true;
       }
-      if(editObject.horizontalFlip) {
+      if (editObject.horizontalFlip) {
         setHorizontalFlip(canvas, context);
         changed = true;
       }
-      if(onFilter && source && changed) onFilter(getExportObject(canvas, source.type))
+      if (onFilter && source) onFilter(getExportObject(canvas, source.type));
     }
-  }
+  };
 
   useEffect(() => {
     setEditObject({
@@ -57,44 +67,47 @@ const PixelsImage: React.FC<PixelsImageProps> = ({
       saturation,
       lastChange: Date.now(),
       verticalFlip,
-      horizontalFlip
-    })
-  }, [filter, brightness, contrast, saturation, verticalFlip, horizontalFlip])
+      horizontalFlip,
+    });
+  }, [filter, brightness, contrast, saturation, verticalFlip, horizontalFlip]);
 
   useEffect(() => {
-    if(ref && ref.current) {
+    if (ref && ref.current) {
       const observer = new IntersectionObserver((ent) => {
-        const v = !!ent.find(e => e.isIntersecting);
-        if(v != isVisible) setIsVisible(v);
-      })
+        const v = !!ent.find((e) => e.isIntersecting);
+        if (v != isVisible) setIsVisible(v);
+      });
       observer.observe(ref.current);
     }
-  }, [ref.current])
+  }, [ref.current]);
 
   useEffect(() => {
-    if(ref.current && data && isVisible && !isFiltered) load();
-  }, [data, isVisible, isFiltered, ref.current])
+    if (ref.current && data && isVisible && !isFiltered) load();
+  }, [data, isVisible, isFiltered, ref.current]);
 
   useEffect(() => {
-    if(ref && ref.current && source) {
+    if (ref && ref.current && source) {
       setIsFiltered(false);
       setData(drawImageSource(ref.current, source));
     }
-  }, [source, ref.current, editObject.lastChange])
+  }, [source, ref.current, editObject.lastChange]);
 
   useEffect(() => {
-    if(src) {
-      if(src instanceof HTMLImageElement || src instanceof HTMLCanvasElement || typeof src === "string") {
+    if (src) {
+      if (
+        src instanceof HTMLImageElement ||
+        src instanceof HTMLCanvasElement ||
+        typeof src === "string"
+      ) {
         (async () => {
           const source = await getImageSource(src, type);
           setSource(source);
         })();
-      }
-      else setSource(src);
+      } else setSource(src);
     }
-  }, [src])
+  }, [src]);
 
-  if(src) return  <canvas {...props} ref={ref} />;
+  if (src) return <canvas {...props} ref={ref} />;
 };
 
 export default PixelsImage;
