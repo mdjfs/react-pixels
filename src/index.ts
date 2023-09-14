af506aabc9b7798ad61687d0a46d8958ede0276b
u632763 <mdfuenmayorsoto@teco.com.ar>
2023-09-14 10:19:35 -0300

    fix imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export { default as PixelsImage } from './PixelsImage';
+import PixelsImage from './PixelsImage';
+export { PixelsImage }
 export type { PixelsImageProps } from "./types"
 export type { PixelsImageSource, EXPORT_OBJECT, FILTERS } from "node-pixels"
 export { getImageSource, getExportObject, FILTERS_LIST } from "node-pixels";
+export default PixelsImage
